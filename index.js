ea2bf5e79b986da7bca760c2b279cf3fba2da30f
CodeHennessy <albesandi1208@gmail.com>
2021-02-13 00:03:10 +0100

    freeCode-JavaScript
    Firsts steps with JavaScript

---
+++ index.js
@@ -0,0 +1,45 @@
+var myName;
+
+var a;
+a = 7;
+
+var a;
+a = 7;
+var b;
+b = a;
+
+var a = 9;
+
+var a=5;
+var b=10;
+var c="I am a";
+
+var studlyCapVar=10;
+var properCamelCase="I am a";
+var titleCaseOver=9000;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+var sum = 10 + 10;
+
+var difference = 45 - 33;
+
+var product = 8 * 10;
+
+var quotient = 66 / 33;
+
+var myVar = 87;
+myVar++;
+
+var myVar = 11;
+myVar--;
+
+var ourDecimal = 5.7;
+var myDecimal = 5.7;
+
+var product = 2.0 * 2.5;
+
+var quotient = 4.4 / 2.0;
+
