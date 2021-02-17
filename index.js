8d41238a9f2a825348b36c0c98389d33ed7b5828
CodeHennessy <albesandi1208@gmail.com>
2021-02-17 01:40:30 +0100

    freeCode-Javascript
    more booleans

--- index.js
+++ index.js
@@ -250,3 +250,161 @@ function welcomeToBooleans() {
   return true;
 }
 
+function trueOrFalse(wasThatTrue) {
+   if (wasThatTrue) {
+     return "Yes, that was true";
+   }
+   return "No, that was false";
+}
+
+function testEqual(val) {
+  if (val==12) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+function testStrict(val) {
+  if (val===7) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+
+function compareEquality(a, b) {
+  if (a === b) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+function testNotEqual(val) {
+  if (val!=99) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+function testStrictNotEqual(val) {
+  if (val!==17) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+
+function testGreaterThan(val) {
+  if (val>100) { 
+    return "Over 100";
+  }
+
+  if (val>10) { 
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+function testGreaterOrEqual(val) {
+  if (val>=20) {
+    return "20 or Over";
+  }
+
+  if (val>=10) {
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
+function testLessThan(val) {
+  if (val<25) {
+    return "Under 25";
+  }
+
+  if (val<55) {
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+function testLessOrEqual(val) {
+  if (val<=12) {
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val<=24) {
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+function testLogicalAnd(val) {
+  if (val<=50 && val>=25){
+      return "Yes";
+  }
+  return "No";
+}
+
+testLogicalAnd(10);
+
+function testLogicalOr(val) {
+  if (val<10 || val>20){
+    return "Outside";
+  }
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+function testLogicalOr(val) {
+  if (val<10 || val>20){
+    return "Outside";
+  }
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+  return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
