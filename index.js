190a810fbde59b17e9b500fdcf71df8739263963
CodeHennessy <albesandi1208@gmail.com>
2021-02-19 02:24:02 +0100

    freeCode-JavaScript
    Updating Object PropertiesPassed

--- index.js
+++ index.js
@@ -533,3 +533,47 @@ var holdbet ='Hold'
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+var myDog = {
+  "name": "Buddy",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Alberto","Rafa","Marivi","Alex"]
+  };
+
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
+
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+var entreeValue = testObj["an entree"];
+var drinkValue = testObj["the drink"];
+
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+var playerNumber=16;
+var player = testObj[playerNumber];
+
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.name="Happy Coder";
+
